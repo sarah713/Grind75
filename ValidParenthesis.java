a7e856443d46b8675fa779624624a92408ebf03c
Sarah <sarahkulsoom18@gmail.com>
2023-09-18 18:54:20 +0530

    Valid Parenthesis

---
+++ ValidParenthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                if (st.isEmpty() || st.peek() != '(') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == '}') {
+                if (st.isEmpty() || st.peek() != '{') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == ']') {
+                if (st.isEmpty() || st.peek() != '[') {
+                    return false;
+                }
+                st.pop();
+            }
+            st.push(c);
+        }
+        return st.isEmpty();
+    }
+}
