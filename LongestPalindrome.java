bb528fb2ec241707b57f82e766cf78c712d5a7bf
Sarah <sarahkulsoom18@gmail.com>
2023-09-21 21:33:03 +0530

    Longest Palindrome

---
+++ LongestPalindrome.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() < 1)
+            return 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i)))
+                set.remove(s.charAt(i));
+            else
+                set.add(s.charAt(i));
+        }
+        if (set.size() <= 1)
+            return s.length();
+        return s.length() - set.size() + 1;
+    }
+}
