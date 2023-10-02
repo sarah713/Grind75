9870fb0516368ca98fcd2cdf7d5830a518c8c813
Sarah <sarahkulsoom18@gmail.com>
2023-10-02 18:11:00 +0530

    Validate Binary Search Tree

---
+++ ValidateBinarySearch.java
@@ -0,0 +1,20 @@
+public class ValidateBinarySearch {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+
+    }
+}
