8ee66008b11003a1903e40030ea87e45975d96f5
Sarah <sarahkulsoom18@gmail.com>
2023-10-10 19:44:43 +0530

    Can Partition

---
+++ canPartition.java
@@ -0,0 +1,34 @@
+public class canPartition {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (sum / 2 != 0) {
+            return false;
+        }
+
+        return subsetSum(nums, sum / 2);
+
+    }
+
+    public boolean subsetSum(int[] nums, int sum) {
+        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[nums.length][sum];
+    }
+}
