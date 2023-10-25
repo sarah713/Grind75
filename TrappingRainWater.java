10f93cf4cc9ffe73cbbbf11ba88da3d9e34495a4
Sarah <sarahkulsoom18@gmail.com>
2023-10-25 23:37:05 +0530

    Trapping Raining water

---
+++ TrappingRainWater.java
@@ -0,0 +1,23 @@
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = height.length - 1;
+        int water = 0;
+        while (left <= right) {
+            if (height[left] > height[right]) {
+                if (height[left] < leftMax) {
+                    water += leftMax - height[left];
+                }
+                leftMax = Math.max(leftMax, height[left]);
+                left++;
+            } else {
+                if (height[right] < rightMax) {
+                    water += rightMax - height[right];
+                }
+                rightMax = Math.max(rightMax, height[right]);
+                right--;
+            }
+        }
+        return water;
+    }
+}
